c49e9252cab147ade79f9a24817bcf037fa6015f
Rohama76 <115659870+Rohama76@users.noreply.github.com>
2023-05-26 13:03:27 +0500

    Add files via upload

---
+++ javascriptone/index.js
@@ -0,0 +1,513 @@
+//chapter#05/
+//pdf #1/
+//question#1//
+var num=+prompt("enter first number for add");
+var first =+prompt("enter second number number for add");
+var two=document.write(first+num);
+console.log(two);
+
+//question#02//
+var three=+prompt("enter first number for subtract");
+var four =+prompt("enter second number for subtract");
+var five=document.write(three-four);
+console.log(five);
+
+
+var six=+prompt("enter first number for multiply");
+var seven =+prompt("enter second number for multiply");
+var eight=document.write(six*seven);
+console.log(eight);
+
+
+var nine=+prompt("enter first number for modulus");
+var ten =+prompt("enter second number for modulus");
+var eleven=document.write(nine%ten);
+console.log(eleven);
+
+
+var three=+prompt("enter first number for divide");
+var four =+prompt("enter second number for divide");
+var five=document.write(three/four);
+console.log(five);
+
+//QUESTION#03//
+var roll;
+console.log(roll);
+document.write("value after variable declaration is:undefine");
+
+roll=24;
+console.log(roll);
+
+document.write("initial value:24");
+var roll=24;
+roll=(roll+1);
+console.log(roll);
+document.write("value after increment is:25");
+var x=roll+7;
+console.log(x);
+document.write("value after addition is:32");
+var x=32;
+x=(x-1);
+console.log(x);
+document.write("value after dcrement is:31");
+var x=31;
+x=x/3;
+console.log(x);
+document.write("reminder is:10.33334");
+
+//question#04/
+var oneticket=600;
+console.log(oneticket);
+var oneticket=600;
+oneticket=600*5;
+console.log(oneticket);
+document.write("cost for 5 tickets is:3000");
+//QUESTION05/
+var a=4;
+var b =1;
+var c=a*b;
+console.log(c);
+document.write("4*1=4");
+
+
+var d=4;
+var e =2;
+var f=d*e;
+console.log(f);
+document.write("4*2=8");
+
+var g=4;
+var h =3;
+var i=g*h;
+console.log(i);
+document.write("4*3=12");
+
+var j=4;
+var k =4;
+var l=j*k;
+console.log(l);
+document.write("4*4=16");
+
+var m=4;
+var n=5;
+var o=m*n;
+console.log(o);
+document.write("4*5=20");
+
+var p=4;
+var q =6;
+var r=p*q;
+console.log(r);
+document.write("4*6=24");
+
+var s=4;
+var t =7;
+var u=s*t;
+console.log(u);
+document.write("4*7=28");
+
+var v=4;
+var w =8;
+var x=v*w;
+console.log(x);
+document.write("4*8=32");
+
+var y=4;
+var z =9;
+var aa=y*z;
+console.log(aa);
+document.write("4*9=36");
+
+var bb=4;
+var cc =10;
+var dd=bb*cc;
+console.log(dd);
+document.write("4*10=40");
+//QUESTION6//
+var tem=2;
+var x=(tem*9/5)+32;
+console.log(x);
+document.write("2cis 35.6f");
+var temp=64;
+var y=(64-32)*5/9;
+console.log(y);
+document.write("64fis 17.77c");
+//QUESTION#07//
+var a=650;
+console.log(a);
+document.write("price of one item is 650");
+var c=100;
+console.log(c);
+document.write("quantity of item is 3");
+var d=650;
+console.log(d);
+document.write("price of 2item is 100");
+var e=7;
+console.log(e);
+document.write("quantity of 2 item is 7");
+var b=100;
+console.log(b);
+document.write("shipping charges 100")
+var x=a+c+d+e+b;
+console.log(x);
+document.write("total coast of your orderis 1507")
+//QUESTION#8//
+var totalmarks=980;
+console.log(totalmarks);
+document.write("totalmarks is:980");
+var obtained=804;
+console.log(obtained);
+document.write("marks obtainedis:804")
+var x=obtained/totalmarks*100;
+console.log(x);
+document.write("percentageis:82.04")
+//question#09//
+var x=(10*104.80)+(25*28)
+console.log(x);
+document.write("total currency in pkris:1748");
+//QUESTION#10//
+var mum;
+num=40;
+var x=num+5*10/2;
+console.log(x);
+document.write("totalis:65");
+//QUESTION11//
+var a=2023;
+console.log(a);
+document.write("current year:2023");
+var b=2007;
+console.log(b);
+document.write("birth year:1002");
+var y=a-b;
+console.log(y);
+document.write("current age:16");
+//QUESATION#12//
+var a=4;
+console.log(a);
+var b=2*3.14*4;
+console.log(b);
+document.write("radius of circle:25.12");
+//question13//
+var a="choclatechip";
+console.log(a);
+document.write("favourite snacks");
+var b=16;
+console.log(b);
+document.write("current age:16");
+var c=50;
+console.log(c);
+document.write("estimated age:50");
+var d=4;
+console.log(d);
+document.write("amount of snacks per day:4");
+var y=(c-b)*4;
+console.log(y);
+document.write("your life time snacks will be:136");
+
+//pdf 2//
+//pdf#2//
+//chapter6-9//
+//question1//
+document.write("result:<br>");
+var a=10
+ document.write("value of ++a:11"+(a++)+"<br>");
+ document.write("now the value of a:"+a+"<br>");
+ document.write("value of a++:" +(a++)+"<br>");
+ document.write("now the value of a:"+a+"<br>");
+ document.write("value of --a:"+(--a)+"<br>")
+ document.write("now the value of a:"+a+"<br>");
+ document.write("value of a--:"+(a--)+"<br>");
+ document.write("now the value of a:"+a+ "<br>");
+//question2
+var a=2;
+var b=1
+console.log(--a);
+console.log(--a - --b)
+console.log(--a - --b + ++b)
+console.log(--a - --b + ++b + b--)
+//question3
+var name=prompt("enter your name");
+alert("good morning");
+//question 4 is not in assigment//
+//question5
+
+const number = parseInt(prompt('Enter an integer: '));
+
+for(let i = 1; i <= 10; i++) {
+
+    const result = i * number;
+
+    console.log(`${number} * ${i} = ${result}`);
+}
+let i = 5;
+while( i = 5)
+    console.log(n + " * " + i +
+        " = " + n * i);
+        //question6
+        var name=document.write(prompt("enter subject "+"<br>"));
+        var name=document.write(prompt("enter subject "+"<br>"));
+        var name=document.write(prompt("enter subject "+"<br>"));
+        var total=document.write("100 "+"<br>");
+        var name1=document.write(prompt("enter your marks of subject 1"+"<br>"));
+        var name2=document.write(prompt("enter your marks of subject 2"+"<br>"));
+        var name3=document.write(prompt("enter your marks of subject 3"+"<br>"));
+var totalmarks=name1+name2+name3;
+
+var percentage=totalmarks/total*100
+console.log(percentage)
+//pdf#3//
+//Q1
+var city = ("karachi");
+var usercity = prompt("enter city")
+if ("city===usercity") { alert("welcome to city of lights") }
+
+//Q2
+var gender = ("male")
+var usergender = prompt("enter gender")
+if ("gender===usergender") { alert("Good morning Sir"); }
+
+var gender = ("female")
+var usergender = prompt("enter gender")
+if ("gender===usergender") { alert("Good morning madam"); }
+
+// //Q3
+var color = prompt("enter traffic signal light color");
+if (color === "red") { alert("must stop"); }
+else if (color === "yellow") { alert("Ready to move"); }
+else if (color === "green") { alert("Move now"); }
+else { alert("it's not a traffic signal light color "); }
+
+//Q4
+var fuelInCar = prompt("enter remaining fuel in car (in litres)");
+if (fuelInCar < 0.25) { alert("please refill the fuel in car"); }
+
+//Q5
+var a = 4;
+if (++a === 5) { alert("given condition for variable a is true "); }//displayed 
+
+var b = 82;
+if (b++ === 83) { alert("given condition for variable b is true "); }//not displayed
+
+var c = 12;
+if (c++ === 13) { alert("condition 1 is true"); }//not displayed
+if (c === 13) { alert("condition 2 is true"); }//not displayed
+if (++c < 14) { alert("condition 3 is true"); }//displayed
+if (c === 14) { alert("condition 4 is true"); }//not displayed
+
+var materialCost = 20000;
+var labourCost = 2000;
+var totalCost = materialCost + labourCost;
+if (totalCost === labourCost + materialCost) { alert("the cost equals"); }//displayed
+
+if (true) { alert("True"); }//displayed
+if (false) { alert("false"); }//not displayed
+
+if ("car" < "cat") { alert("car is smaller than cat"); }//displayed
+
+//Q6
+document.write("<h3>Marks Sheet<h3/>")
+var maths = prompt("Enter Your Maths Marks");
+var chem = prompt("Enter Your Chem Marks");
+var eng = prompt("Enter Your Eng Marks");
+var obtained_marks = parseInt(maths) + parseInt(chem) + parseInt(eng);
+var percentage = obtained_marks * 100 / 500
+
+document.write("Total Marks: 500 <br/>")
+document.write("Obtained Marks: " + obtained_marks + "<br/>");
+document.write("Percentage: " + percentage + "<br/>");
+
+if (percentage >= 80) {
+    document.write("Grade: A+ <br/>");
+}
+else if (percentage >= 70) {
+    document.write("Grade: A <br/>");
+}
+else if (percentage >= 60) {
+    document.write("Grade: B <br/>");
+}
+else if (percentage >= 50) {
+    document.write("Grade: C <br/>");
+}
+else if (percentage >= 40) {
+    document.write("Grade: D <br/>");
+}
+else {
+    document.write("You Are Fail!! <br/>");
+}
+
+/* if else if of remarks */
+if (percentage >= 80) {
+    document.write("Remarks: Excellent");
+}
+else if (percentage >= 70) {
+    document.write("Remarks: Very Good");
+}
+else if (percentage >= 60) {
+    document.write("Remarks: Good");
+}
+else if (percentage >= 50) {
+    document.write("Remarks: Fair");
+}
+else if (percentage >= 40) {
+    document.write("Remarks: Needs Improvement");
+}
+else {
+    document.write("Remarks: Disqualified");
+}
+/* if else if of remarks */
+
+//Q7
+var num = 7;
+var guess = prompt("guess the number.");
+if (guess === num) { alert("Bingo! Correct answer."); }
+else if (guess === 8) { alert("close enough to correct answer."); }
+
+//Q8
+var num = prompt("Enter a Number");
+var modulus = num % 3;
+if (modulus === 0) {
+    alert(num + " is divisible by 3");
+}
+
+//Q9
+var num = prompt("Enter a Number");
+var modulus = num % 2;
+if (modulus === 0) {
+    alert(num + " is an even number");
+}
+else {
+    alert(num + " is an odd number");
+}
+
+//Q10
+var temperature = prompt("Enter Temperature");
+if (temperature > "40") {
+    alert("It is  hot .");
+}
+else if (temperature > "30") {
+    alert("Today Weather  is Normal.");
+}
+else if (temperature > "20") {
+    alert("Today’s Weather is cool.");
+}
+else if (temperature > "10") {
+    alert(" Today’s weather is                                        Cool.");
+}
+
+//Q11
+var first_number = +prompt("Enter First Number");
+var second_number = +prompt("Enter Second Number");
+var operation = prompt("Enter Operator");
+var addition = first_number + second_number;
+var substract = first_number - second_number;
+var multiply = first_number * second_number;
+var division = first_number / second_number;
+var modulus = first_number % second_number;
+if (operation === "+") {
+    alert("The Answer is: " + addition);
+}
+else if (operation === "-") {
+    alert("The Answer is: " + substract);
+}
+else if (operation === "*") {
+    alert("The Answer is: " + multiply);
+}
+else if (operation === "/") {
+    alert("The Answer is: " + division);
+}
+else if (operation === "%") {
+    alert("The Answer is: " + modulus);
+}
+//last pdf//
+//question1//
+var userinput=prompt("enter something")
+if(userinput>=65 && userinput<=90){
+    console.log("capital letter")
+}
+else if(userinput>=97 && userinput<=122){
+    console.log("small letter")
+}
+//Question 2//
+var num=prompt("enter number")
+var num2=prompt("enter number")
+
+if(num<num2){
+    alert("it is greater")
+}if(num=num2){
+    alert("it is equal")
+}
+//question3//
+const num=prompt("enter number")
+if(num>0){ console.log("the numberis positive")}
+else if (num==0){console.log("the number is zero")}
+else{
+    console.log("the number is negative")
+}
+//question4
+var a=towel
+var num=prompt("enter ")
+
+if(a===num){
+    alert("true")
+}if(a!=num){
+    alert("false")
+}
+//question5//
+var password = "Correctpassword";
+var user_password = prompt("enter your Password");
+if (user_password === "") {
+    alert("Please enter your password");
+} else if (user_password === password) {
+    alert("Correct! The password you entered matches the original password");
+} else if (user_password != password) {
+    alert("Incorrect password");
+}
+
+//Q6
+var hour = prompt("enter current hour");
+// var hour = 13;
+if (hour < 18) {
+    alert
+        ("good day")
+}
+else if (hour > 18) {
+    alert("good evening")
+}
+
+
+//Q7
+var time = parseInt(prompt("enter time in 24 hour clock format"));
+if (time >= 0000 && time < 1200) {
+    alert("Good Morning!");
+} else if (time >= 1200 && time < 1700) {
+    alert("Good Afternoon!");
+} else if (time >= 1700 && time < 2100) {
+    alert("Good Evening!");
+} else if (time >= 2100 && time <= 2359) {
+    alert("Good Night!");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
